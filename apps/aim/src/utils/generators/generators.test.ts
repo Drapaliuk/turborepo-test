ff42d1339f8b0b62d9021a2dc63d179bd0e4172c
Vitalii Drapaliuk <ghvitaliidrapaliuk@gmail.com>
2025-04-23 10:11:12 +0200

    added tests

---
+++ apps/aim/src/utils/generators/generators.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from 'vitest';
+
+
+
+describe('Generators', () => {
+    it('Test genrator', () => {
+        expect(1).equal(1)
+    })
+});
---
+++ apps/broadcaster-portal/src/utils/array/unique/unique.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest';
+
+describe('Array unique', () => {
+    it('Test unique', () => {
+        expect(1).equal(1)
+    })
+});
