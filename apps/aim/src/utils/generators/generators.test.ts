19effd8fc5694e456df3e03bfe4299504ed0a6eb
Vitalii Drapaliuk <ghvitaliidrapaliuk@gmail.com>
2025-04-23 10:23:03 +0200

    changes

--- apps/aim/src/utils/generators/generators.test.ts
+++ apps/aim/src/utils/generators/generators.test.ts
@@ -4,7 +4,6 @@ import { describe, expect, it } from 'vitest';
 
 describe('Generators', () => {
     it('Test genrator', async () => {
-
         await new Promise(res => setTimeout(res, 4000))
         expect(1).equal(1)
     })
