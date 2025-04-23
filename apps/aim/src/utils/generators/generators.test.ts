25209e3733dd4e062a0330d2d0a75622920b5c74
Vitalii Drapaliuk <ghvitaliidrapaliuk@gmail.com>
2025-04-23 10:15:29 +0200

    added slepp in test

--- apps/aim/src/utils/generators/generators.test.ts
+++ apps/aim/src/utils/generators/generators.test.ts
@@ -3,7 +3,9 @@ import { describe, expect, it } from 'vitest';
 
 
 describe('Generators', () => {
-    it('Test genrator', () => {
+    it('Test genrator', async () => {
+
+        await new Promise(res => setTimeout(res, 4000))
         expect(1).equal(1)
     })
 });
