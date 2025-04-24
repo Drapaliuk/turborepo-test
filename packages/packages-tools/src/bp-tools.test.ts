33de87e7727038044569fc252540bb799c316921
Vitalii Drapaliuk <ghvitaliidrapaliuk@gmail.com>
2025-04-24 10:11:45 +0200

    ad

---
+++ packages/packages-tools/src/bp-tools.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest';
+
+describe('___BP__TOOLS___', () => {
+    it('Test bpTool', async () => {
+        expect(1).equal(1)
+    })
+});
---
+++ packages/packages-tools/src/bp-tools.ts
@@ -0,0 +1,3 @@
+export const bpTool = () => {
+    return 'Broadcaster portal tool result'
+};
