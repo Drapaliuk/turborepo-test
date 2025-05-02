bdfe22889e7a18c453480e1699d863bd31e4bbf4
Vitalii Drapaliuk <ghvitaliidrapaliuk@gmail.com>
2025-05-02 09:17:50 +0200

    asd

--- apps/aim/bin/vercel-ignore.sh
+++ apps/aim/bin/vercel-ignore.sh
@@ -2,10 +2,12 @@
 
 BRANCH="$VERCEL_GIT_COMMIT_REF"
 
-if [ "$BRANCH" = "main" ]; then
-  echo "🛑 - Build cancelled"
+if turbo ls --filter="[HEAD]..." | grep -qE '(^|/)aim$|apps/aim'; then
+  echo "✅ aim or apps/aim was affected"
   exit 0
 else
-  echo "✅ - Build can proceed"
+  echo "🚫 No changes in aim or apps/aim"
   exit 1
 fi
+
+
