beeeea200481815622802777ddcfee324a26a706
Vitalii Drapaliuk <ghvitaliidrapaliuk@gmail.com>
2025-05-02 10:19:12 +0200

    added new line

--- apps/broadcaster-portal/src/App.vue
+++ apps/broadcaster-portal/src/App.vue
@@ -10,7 +10,7 @@ import { capitalize } from '@repo/strings/capitalize';
   <h1>{{ capitalize('Broadcaster portal Project') }}</h1>
   <div>Check math 1+1: {{ add(1, 1) }}</div>
   <div>{{ bpTool() }}</div>
-  
+  <div>New line</div>
 </template>
 
 <style scoped></style>
